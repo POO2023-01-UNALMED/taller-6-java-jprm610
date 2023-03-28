8798ce28d68259133e9be5018182755ba46a58c4
jprm610 <jprm610@gmail.com>
2023-03-28 00:25:02 -0500

    3

--- src/main/java/vehiculos/Automovil.java
+++ src/main/java/vehiculos/Automovil.java
@@ -7,6 +7,7 @@ public class Automovil extends Vehiculo {
     public Automovil(String placa, String nombre, int precio, int peso, Fabricante fabricante, int puestos) {
         super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante);
         this.puestos = puestos;
+        cantidadAutomoviles++;
     }
 
     // GETTERS AND SETTERS
--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -9,6 +9,7 @@ public class Camioneta extends Vehiculo {
     public Camioneta(String placa, int puertas, String nombre, int precio, int peso, Fabricante fabricante, boolean volco) {
         super(placa, puertas, 90, nombre, precio, peso, "4X4", fabricante);
         this.volco = volco;
+        cantidadCamionetas++;
     }
 
     // GETTERS AND SETTERS
